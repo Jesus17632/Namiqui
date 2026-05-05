8c873ff534d4eba3659ee91f0cbef7ae515c732c
olmos <hernol002@gmail.com>
2026-05-04 23:06:33 -0600

    Primer commit: Estructura inicial de ResiApp

---
+++ ResiApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ResiApp
+//
+//  Created by Dev Jr.23 on 5/4/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ResiApp/ResiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResiAppApp.swift
+//  ResiApp
+//
+//  Created by Dev Jr.23 on 5/4/26.
+//
+
+import SwiftUI
+
+@main
+struct ResiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
